b2b8ad946dd1cff328c3bf63d5685df7e42d363f
bujosa <davidbujosa@gmail.com>
2022-12-18 21:00:32 -0400

    feat: add example with shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     println!("Hello, world! {}", add(2, 2));
+
+    // Example with shadowing
+    let shadow_num = 5;
+    let shadow_num = shadow_num + 5;
+    let shadow_num = shadow_num * 2;
+    println!("The value of shadow_num is: {}", shadow_num);
 }
 
 fn add(a: i32, b: i32) -> i32 {
