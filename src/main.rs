a53a11f57a625ecd0b5292ece70e87c15c208d2b
bujosa <davidbujosa@gmail.com>
2022-12-21 03:08:03 -0400

    feat: add example with enum

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ fn main() {
     let shadow_num = shadow_num + 5;
     let shadow_num = shadow_num * 2;
     println!("The value of shadow_num is: {}", shadow_num);
+
+    #[derive(Debug)]
+    enum Book {
+        Paperback(bool),
+    }
+
+    let book = Book::Paperback(true);
+    println!("Book: {:#?}", book);
 }
 
 fn add(a: i32, b: i32) -> i32 {
